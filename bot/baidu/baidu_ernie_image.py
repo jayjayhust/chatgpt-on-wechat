6eeed1a085d26eddbe7209270b390d4135dc54c0
jayjayhust <jayhust@163.com>
2023-11-02 19:57:55 +0800

    recitfy the base64 jpeg image sending logic, test ok

--- bot/baidu/baidu_ernie_image.py
+++ bot/baidu/baidu_ernie_image.py
@@ -33,7 +33,7 @@ class BaiduErnieImage(object):
             payload = json.dumps({
                 "prompt": query,
                 "negative_prompt": "",
-                "size": conf().get("image_create_size", "512x512"),  # ["512x512", "768x768", "768x1024", "1024x768", "576x1024", "1024x576", "1024x1024"]
+                "size": conf().get("image_create_size", "1024x1024"),  # ["512x512", "768x768", "768x1024", "1024x768", "576x1024", "1024x576", "1024x1024"]
                 "steps": 20, 
                 "n": 1,
                 "sampler_index": "DPM++ SDE Karras" 
@@ -43,10 +43,18 @@ class BaiduErnieImage(object):
             }
             response = requests.request("POST", url, headers=headers, data=payload)
 
-            if "id" in response.json():  # 形如：https://cloud.baidu.com/doc/WENXINWORKSHOP/s/Klkqubb9w#%E5%93%8D%E5%BA%94%E7%A4%BA%E4%BE%8B
-                return True, response.json()["data"][0]["b64_image"]  # 图片base64数据
-            elif "error_code" in response.json():  # 服务器返回错误信息
-                return False, response.json()["error_msg"]
+            if 'id' in response.json():  # 形如：https://cloud.baidu.com/doc/WENXINWORKSHOP/s/Klkqubb9w#%E5%93%8D%E5%BA%94%E7%A4%BA%E4%BE%8B
+                # 参考示例：https://blog.csdn.net/m0_46825740/article/details/127869841
+                import base64
+                import hashlib
+                image = base64.b64decode(response.json()['data'][0]['b64_image'], altchars=None, validate=False)  # reply.content形如"/9j/4AAQSkZJRg..."
+                uid = hashlib.md5(str(image).encode()).hexdigest()  # 根据图片文件内容做md5计算，获得唯一id
+                file_path = './tmp/' + uid + '.jpg'  # 根目录下的tmp文件夹
+                with open(file_path, mode='wb') as file:  # 新建文件并写入
+                    file.write(image)
+                return True, file_path  # 图片base64的文件地址
+            elif 'error_code' in response.json():  # 服务器返回错误信息
+                return False, response.json()['error_msg']
         except Exception as e:
             logger.exception(e)
             return False, str(e)
