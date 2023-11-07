3d103e8ac852b4ce03a48785ab5b93f47a876261
jayjayhust <jayhust@163.com>
2023-11-07 11:58:33 +0800

    temporary change the text_abstract engine into chatglm_turbo while using ernie_bot_turbo for chat

--- channel/chat_channel.py
+++ channel/chat_channel.py
@@ -349,7 +349,8 @@ class ChatChannel(Channel):
                         reply.type = ReplyType.TEXT
                         reply.content = '抱歉，您分享的文章内容过长，暂时无法生成摘要。敬请期待我的能力升级吧，阿图fighting~'
                         return reply
-                    elif (conf().get("model", "") == "ernie_bot_turbo") and (len(text) > 6500):
+                    # elif (conf().get("model", "") == "ernie_bot_turbo") and (len(text) > 6500):
+                    elif (conf().get("model", "") == "ernie_bot_turbo") and (len(text) > 32000):
                         logger.debug('Text in this shared link is too long!')
                         reply.type = ReplyType.TEXT
                         reply.content = '抱歉，您分享的文章内容过长，暂时无法生成摘要。敬请期待我的能力升级吧，阿图fighting~'
