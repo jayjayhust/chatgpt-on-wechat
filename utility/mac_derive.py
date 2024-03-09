9ebcc269de1e091651af781614c861658db81547
jayjayhust <jayhust@163.com>
2024-03-09 21:33:39 +0800

    update group chat daily limitation for bot

--- utility/mac_derive.py
+++ utility/mac_derive.py
@@ -14,6 +14,7 @@ import uuid
 # 方法2
 from psutil import net_if_addrs
 
+from common.log import logger
 
 # 获取所有MAC地址
 def list_all_mac():
@@ -46,25 +47,39 @@ class mac_derive(object):
     # 打印所有MAC地址和IP地址
     def list_all_mac_and_ip(self):
         for k, v in net_if_addrs().items():
-            print('*' * 100)
-            print(k)  # 输出形如：以太网/vEthernet (Default Switch)/本地连接* 1/VMware Network Adapter VMnet1/WLAN/Loopback Pseudo-Interface 1
+            logger.debug('*' * 100)
+            logger.debug(k)  # 输出形如：以太网/vEthernet (Default Switch)/本地连接* 1/VMware Network Adapter VMnet1/WLAN/Loopback Pseudo-Interface 1
             for item in v:
                 address = item[1]
                 if '-' in address and len(address)==17:
-                    print(address)  # 输出形如：B0-25-AA-4C-55-AC
+                    logger.debug(address)  # 输出形如：B0-25-AA-4C-55-AC
                 if '.' in address:
-                    print(address)  # 输出形如：192.168.0.28
+                    logger.debug(address)  # 输出形如：192.168.0.28
 
     # 获取无线网卡MAC地址
     def get_wlan_mac(self):
         for k, v in net_if_addrs().items():
-            print('*' * 100)
-            print(k)  # 输出形如：以太网/vEthernet (Default Switch)/本地连接* 1/VMware Network Adapter VMnet1/WLAN/Loopback Pseudo-Interface 1
+            logger.debug('*' * 100)
+            logger.debug(k)  # 输出形如：以太网/vEthernet (Default Switch)/本地连接* 1/VMware Network Adapter VMnet1/WLAN/Loopback Pseudo-Interface 1
             if k in ["WLAN"]:  # 类型为无线网卡
                 for item in v:
                     address = item[1]
                     if '-' in address and len(address)==17:
-                        print(address)  # 输出形如：B0-25-AA-4C-55-AC
+                        logger.debug(address)  # 输出形如：B0-25-AA-4C-55-AC
+                        return address
+        # return None
+        return "00-00-00-00-00-00"
+    
+    # 获取有线网卡MAC地址
+    def get_ethernet_mac(self):
+        for k, v in net_if_addrs().items():
+            logger.debug('*' * 100)
+            logger.debug(k)  # 输出形如：以太网/vEthernet (Default Switch)/本地连接* 1/VMware Network Adapter VMnet1/WLAN/Loopback Pseudo-Interface 1
+            if k in ["以太网"]:  # 类型为无线网卡
+                for item in v:
+                    address = item[1]
+                    if '-' in address and len(address)==17:
+                        logger.debug(address)  # 输出形如：B0-25-AA-4C-55-AC
                         return address
         # return None
         return "00-00-00-00-00-00"
